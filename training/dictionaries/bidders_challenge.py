57c306bf328c9addfd863328b4c7a1a81f274709
Valerius-ufv <divasa2001@gmail.com>
2024-09-18 18:28:44 +0200

    Mis primeros avances con Git

--- training/dictionaries/bidders_challenge.py
+++ training/dictionaries/bidders_challenge.py
@@ -13,24 +13,35 @@ def clear():
     # DOS and Windows('nt'): Microsoft's DOS had the cls command to clear the screen. Windows, which initially started as a graphical layer over DOS, maintained a lot of command-line compatibility with DOS for ease of transition and backward compatibility. So, when Windows NT(New Technology) came about, it kept the cls command for clearing the screen.
     # Unix and Unix-like Systems: Unix is an OS that dates back to the 1970s. The command-line interface for Unix used the clear command. Many modern OSes, including Linux and macOS, are derived or inspired by Unix, so they keep the clear command for compatibility and tradition.
 
-
+clear()
 # Start by printing the provided logo.
 print(logo)
 
 # Create a dictionary to hold the bids.
+seguir = input("¿Quieres hacer una puja? (Y/N)")
 
-# TODO: Set an initial state for the bidding process.
+bids_dict_old = {}
 
-# TODO: Create a function to determine the highest bidder.
+while seguir == "Y":
 
+    name = input("¿Cúal es el nombre del pujador?")
+    puja = int(input("¿Cúal es la cantidad apostada?"))
+    bids_dict_old[name] = puja
+    print(bids_dict_old)
+    seguir = input("¿Quieres hacer una puja? (Y/N)")
 
 def find_highest_bidder(bidding_record):
-    # Initialize the necessary variables to determine the winner.
-
-    # Loop through the dictionary to determine the highest bid.
-
+    # Initialize the necessary variables to determine the winner
+    nombre_ganador = None
+    valor_ganador = 0
+    # Loop through the dictionary to determine the highest bid
+    for clave, valor in bids_dict_old.items():
+        if valor > valor_ganador:
+            valor_ganador = valor
+            nombre_ganador = clave
+    
     # Print out the highest bidder.
+    return nombre_ganador, valor_ganador
 
-    # TODO: Use a loop to continue receiving bids until there are no more bidders.
-
-    # Remember to clear the console after each bid to keep the current bid private.
+ganador_final, cantidad_ganador_final = find_highest_bidder(bids_dict_old)
+print("El ganador de la subasta es", ganador_final , "y su puja fue de",  cantidad_ganador_final, ".")
